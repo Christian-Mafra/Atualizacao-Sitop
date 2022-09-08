1af97bc5586bfc8d645392f0071bfda023966e4f
Christian Mafra <18mafra18@gmail.com>
2022-09-08 17:59:09 -0300

    Atualização Sitop

--- src/main/java/christian/mafra/org/robo/Main.java
+++ src/main/java/christian/mafra/org/robo/Main.java
@@ -2,9 +2,16 @@ package christian.mafra.org.robo;
 //Christian Mafra
 //Atualização Sitops
 
-import java.awt.AWTException;
-import java.awt.Robot;
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
 
 public class Main {
     private static Robot robo;
@@ -17,12 +24,12 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) throws AWTException {
+    public static void main(String[] args) throws AWTException, IOException, UnsupportedFlavorException {
 
         //Abrir email
         robo.mouseMove(560,750);
         Click(1);
-        robo.delay(3000);
+        robo.delay(30000);
 
         //Mais opções p/ acessar as pastas
         ClickMais();
@@ -58,22 +65,23 @@ public class Main {
         robo.mouseMove(1320, 140);
         robo.delay(500);
         Click(2);
-        robo.delay(5000);
+        robo.delay(10000);
 
         //---------------------------------------------------------
         //Puchando os Dados
 
         //Aba odn2
-        CopiarData(618,366, 676);
-        Colar(1150,115);
+        //CopiarData(618,366, 676);
+        //Colar(1150,115);
         //Copiando objetivo
         robo.mouseMove(300,524);
         Click(2);
+        robo.delay(500);
         Copiar();
-        Colar(1260,115);
+        Colar(900,115);
+        AlterandoAba(320,25);
 
         //Aba odn1
-        AlterandoAba(320,25);
         CopiarData(620,355, 680);
         Colar(1150,97);
         robo.mouseMove(280,524);
@@ -105,11 +113,89 @@ public class Main {
         robo.keyRelease(KeyEvent.VK_CONTROL);
         robo.keyRelease(KeyEvent.VK_S);
 
+        //COPIAR E COLAR NO TEAMS
+        robo.mouseMove(1300,17);
+        Click(1);
+        robo.mouseMove(1,50);
+        robo.mousePress(KeyEvent.BUTTON1_DOWN_MASK);
+        robo.mouseMove(2,170);
+        robo.mouseRelease(KeyEvent.BUTTON1_DOWN_MASK);
+        Copiar();
+
         //FEICHANDO O DOCUMENTO .TXT
         robo.keyPress(KeyEvent.VK_ALT);
         robo.keyPress(KeyEvent.VK_F4);
         robo.keyRelease(KeyEvent.VK_ALT);
         robo.keyRelease(KeyEvent.VK_F4);
+        robo.delay(500);
+
+        //Abrindo o teams
+        robo.mouseMove(612,752);
+        Click(1);
+        robo.delay(30000);
+        robo.mouseMove(500,30);
+        Click(1);
+        robo.delay(500);
+        robo.keyPress(KeyEvent.VK_D);
+        robo.keyPress(KeyEvent.VK_A);
+        robo.keyPress(KeyEvent.VK_I);
+        robo.keyPress(KeyEvent.VK_L);
+        robo.keyPress(KeyEvent.VK_Y);
+        robo.delay(500);
+        robo.mouseMove(700,180);
+        Click(1);
+        robo.delay(4000);
+        Colar(700,660);
+
+
+        /*
+        ////////
+        String frase, fraseNorbe6, FraseNorbe8, FraseNorbe9, FraseOdn1, FraseOdn2 = "null";
+        String n6Data="null";
+        String n6Objetivo="null";
+
+        String n8Data = "null";
+        String n8Objetivo = "null";
+
+        String n9Data = "null";
+        String n9Objetivo = "null";
+
+        String od2Data = "null";
+        String od2Objetivo = "null";
+
+        String od3Data = "null";
+        String od3Objetivo = "null";
+
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        if (clipboard.isDataFlavorAvailable(DataFlavor.stringFlavor)) {
+            clipboard.getData(DataFlavor.stringFlavor).toString();
+        }
+
+
+       try {
+            if (clipboard.isDataFlavorAvailable(DataFlavor.stringFlavor)) {*/
+                //clipboard.getData(DataFlavor.stringFlavor).toString();
+
+        /*   } else if (clipboard.isDataFlavorAvailable(DataFlavor.javaFileListFlavor)) {
+                List<File> files = (List<File>) clipboard.getData(DataFlavor.javaFileListFlavor);
+
+            } else if (clipboard.isDataFlavorAvailable(DataFlavor.imageFlavor)) {
+                BufferedImage image = (BufferedImage) clipboard.getData(DataFlavor.imageFlavor);
+            }
+
+        } catch (UnsupportedFlavorException | IOException ufe) {
+            Logger.getLogger(getClass().getName()).log(null, null, ufe);
+        }
+
+        frase = ("no6 - Término da intervenção previsto para "+ n6Data +" - "+ n6Objetivo +
+                "no8 - Término da intervenção previsto para "+ n8Data +" - "+ n8Objetivo +
+                "no9 - Término da intervenção previsto para "+ n9Data +" - "+ n9Objetivo +
+                "no9 - Término da intervenção previsto para "+ n9Data +" - "+ n9Objetivo +
+                "no9 - Término da intervenção previsto para "+ n9Data +" - "+ n9Objetivo );
+
+                System.out.println(frase);
+
+         */
 
     }
 
@@ -120,7 +206,6 @@ public class Main {
         robo.keyRelease(KeyEvent.VK_C);
         robo.delay(500);
     }
-
     private static void Colar(int x, int y) {
         robo.mouseMove(x, y);
         Click(2);
